f46e3540ff6bee78b0d07c9cc6e39a0d27b7e192
Hoornaert-Maxime <hoornaert.maxime1@gmail.com>
2023-11-29 13:27:07 +0100

    ajout de la 1er version de la selection des 3 flux

--- controllers/recuperationRss.php
+++ controllers/recuperationRss.php
@@ -40,20 +40,72 @@
             }
         }
 
-        $urlSport = "https://www.lemonde.fr/sport/rss_full.xml";
-        chargerFluxRSS($urlSport);
+        // $urlSport = "https://www.lemonde.fr/sport/rss_full.xml";
+        // chargerFluxRSS($urlSport);
 
-        $urlPlanet ="https://www.lemonde.fr/planete/rss_full.xml";
-        chargerFluxRSS($urlPlanet);
+        // $urlPlanet ="https://www.lemonde.fr/planete/rss_full.xml";
+        // chargerFluxRSS($urlPlanet);
 
-        $urlGeek ="https://www.lemonde.fr/pixels/rss_full.xml";
-        chargerFluxRSS($urlJeuxvideo);
+        // $urlGeek ="https://www.lemonde.fr/pixels/rss_full.xml";
+        // chargerFluxRSS($urlJeuxvideo);
 
-        $urlEconomie ="https://www.lemonde.fr/economie/rss_full.xml";
+        // $urlEconomie ="https://www.lemonde.fr/economie/rss_full.xml";
 
-        $urlScience ="https://www.lemonde.fr/sciences/rss_full.xml";
+        // $urlScience ="https://www.lemonde.fr/sciences/rss_full.xml";
 
-?>
+
+
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $choixUtilisateur = isset($_POST["choix_flux_rss"]) ? $_POST["choix_flux_rss"] : [];
+
+            
+            if (count($choixUtilisateur) > 3) {
+                echo "Erreur : Vous ne pouvez choisir que jusqu'à 3 flux RSS.";
+            } else {
+                
+                foreach ($choixUtilisateur as $choix) {
+                    switch ($choix) {
+                        case "choix1":
+                            $urlChoisi = "https://www.lemonde.fr/sport/rss_full.xml";
+                            chargerFluxRSS($urlChoisi);
+                            break;
+                        case "choix2":
+                            $urlChoisi = "https://www.lemonde.fr/planete/rss_full.xml";
+                            chargerFluxRSS($urlChoisi);
+                            break;
+                        case "choix3":
+                            $urlChoisi = "https://www.lemonde.fr/pixels/rss_full.xml";
+                            chargerFluxRSS($urlChoisi);
+                            break;
+                        case "choix4":
+                            $urlChoisi = "https://www.lemonde.fr/economie/rss_full.xml";
+                            chargerFluxRSS($urlChoisi);
+                            break;
+                        case "choix5":
+                            $urlChoisi = "https://www.lemonde.fr/sciences/rss_full.xml";
+                            chargerFluxRSS($urlChoisi);
+                            break;
+                        default:
+                            echo "Erreur : choix non valide.";
+                    }
+                }
+            }
+        }
+        ?>
+
+        <!-- Formulaire HTML -->
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <label>Choisissez jusqu'à 3 flux RSS :</label><br>
+            <input type="checkbox" name="choix_flux_rss[]" value="choix1"> Sport<br>
+            <input type="checkbox" name="choix_flux_rss[]" value="choix2"> Planète<br>
+            <input type="checkbox" name="choix_flux_rss[]" value="choix3"> Geek<br>
+            <input type="checkbox" name="choix_flux_rss[]" value="choix4"> Économie<br>
+            <input type="checkbox" name="choix_flux_rss[]" value="choix5"> Sciences<br>
+
+            <input type="submit" value="Afficher les flux RSS">
+        </form>
+
 
 </body>
 </html>
